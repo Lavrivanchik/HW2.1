f058c54d7115c163d01ebaf459f60a5062510d14
Иван Лавренюк <Lavrivanchik@yandex.ru>
2024-11-01 16:30:26 +0900

    Домашнее задание 2.1

---
+++ src/Main.java
@@ -0,0 +1,84 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //Homework 2.1
+
+        //Задание 1.
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        //Задание 2.
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        //Задание 3.
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        //Задание 4.
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        //Задание 5.
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        //Задание 6.
+        var fighterOne = 78.2;
+        var fighterTwo = 82.7;
+        System.out.println(fighterOne + fighterTwo);
+        System.out.println(fighterOne - fighterTwo);
+
+        //Задание 7.
+        var remainder = fighterTwo % fighterOne;
+        System.out.println(remainder);
+
+
+        //Задание 8.1.
+        var worker = 8;
+        var workHour = 640;
+        System.out.println("Всего работников в компании " + workHour / worker + " человек");
+
+        //Задание 8.2.
+        var workers = 80;
+        workers = workers + 94;
+        System.out.println("Если в компании работает " + workers + " человека, то всего " + worker * workers + " часов работы может быть поделено между сотрудниками ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
